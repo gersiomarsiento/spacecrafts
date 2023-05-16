14645c61122b5a6e8c3806c930df4ad4a1360c18
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-05-15 23:32:13 -0300

    Fixed home path

--- routes/ships.js
+++ routes/ships.js
@@ -5,7 +5,7 @@ const catchAsyncError = require('../utils/catchAsyncError');
 
 
 //Define routes
-router.get('/index', catchAsyncError(ships.showIndex)); //Show full spaceship list
+router.get('/', catchAsyncError(ships.showIndex)); //Show full spaceship list
 router.get('/add', ships.addSpacecraft); //Show add spaceship page
 router.post('/add', catchAsyncError(ships.postSpacecraft)); //Add new spaceship
 router.all('*', (req, res)=>{ // Define not found error
